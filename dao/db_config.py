a8aa0c7422548fa77f8cf47d9569bc7992266167
Daniel Maia <daniel.maia.sousa@gmail.com>
2025-11-21 16:42:55 -0300

    atualização

--- dao/db_config.py
+++ dao/db_config.py
@@ -1,7 +1,7 @@
 import sqlite3
 import psycopg2
 
-#path / url de conexão
+#path / url de conexãos
 DB_PATH = 'postgresql://neondb_owner:npg_WIxGylq80cOg@ep-red-queen-ac9w25m0-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require'
     
 def get_db_connection():
